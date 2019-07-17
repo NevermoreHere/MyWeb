63dbed7c22f02ed07762ef94a2eca2e4c94a0dfe
mac <xulide12345@gmail.com>
2019-07-17 21:42:24 +0800

    fix

--- model/user.py
+++ model/user.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-from  utils.config import USER_TYPE
+from utils.config import USER_TYPE
 
-class user_model():
-    def __init__(self, type, user_name, user_password):
-        type = USER_TYPE
+
+class user_model:
+    def __init__(self, user_name, user_password):
+        self.type = USER_TYPE
+        self.user_name = user_name
+        self.user_password = user_password
+
+    def get_info(self):
+        print self.type, self.user_name, self.user_password
